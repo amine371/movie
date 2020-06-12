fef78a52b93ff372ff3bfdbe11c5ed1e99c746bb
todo-list hookschckpoint <63712749+amine371@users.noreply.github.com>
2020-06-12 15:14:54 +0100

    Update Movie.js

--- src/composent/Movie.js
+++ src/composent/Movie.js
@@ -1,15 +1,28 @@
 import react from "react";
-const Movie =props =>{
-    const n = props.rating;
-    const rating=[Array(n)].map((e,i)=><span className="play"></span>)
-    return(
-        <div className="Movie">
-            <div>
-                <img width="250" alt={props.title} src={props.poster}/>
-                <h1>{props.title}({props.year})</h1>
-            </div>
-            <p className="rating">{rating}</p>
-        </div>
-    );
+const Movie = ({ starIndex = () => {}, rating }) => {
+  const stars = (x) => {
+    let star = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < x) {
+        star.push(
+          <span className='rating' onClick={() => starIndex(i + 1)}>
+            ★
+          </span>
+        );
+      } else {
+        star.push(
+          <span
+            className='rating'
+            style={{ cursor: 'pointer' }}
+            onClick={() => starIndex(i + 1)}
+          >
+            ☆
+          </span>
+        );
+      }
+    }
+    return star;
+  };
+  return <div>{stars(rating)}</div>;
 };
 export default Movie;
