58c281d635bee61f319efb060d76a3ad1573af14
todo-list hookschckpoint <63712749+amine371@users.noreply.github.com>
2020-06-12 16:01:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
-import MoviesContainer from './movies/MoviesContainer.js';
+import React, { useState } from "react";
+
+import { moviesData } from "./Components/moviesData";
+import Search from "./Components/SearchMovie/Search";
+import MoviesList from "./Components/MoviesList";
+import AddMovie from "./Components/AddMovie/AddMovie";
+
+import "./App.css";
 
 function App() {
+  const [moviesList, setMoviesList] = useState(moviesData);
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState("");
+  const [image, setImage] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [nameSearch, setNameSearch] = useState("");
+  const [ratingSearch, setRatingSearch] = useState(1);
+
+  const addMovie = e => {
+    e.preventDefault();
+    let newMovie = {
+      name: name,
+      rating: rating,
+      image: image,
+      date: date,
+      description: description
+    };
+    return setMoviesList([...moviesList, newMovie]);
+  };
+
   return (
-    
-  <MoviesContainer />
+    <div className="App">
+      <Search
+        setNameSearch={setNameSearch}
+        setRatingSearch={setRatingSearch}
+        ratingSearch={ratingSearch}
+      />
+      <MoviesList
+        moviesData={moviesList}
+        nameSearch={nameSearch}
+        ratingSearch={ratingSearch}
+      />
+      <AddMovie
+        addMovie={addMovie}
+        setName={setName}
+        setRating={setRating}
+        setImage={setImage}
+        setDate={setDate}
+        setDescription={setDescription}
+      />
+    </div>
   );
 }
 
-export default App
+export default App;
